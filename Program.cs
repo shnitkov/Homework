dcd95a493902a395339c7da2cafc3928abb8fbd2
shnitkov <129221961+shnitkov@users.noreply.github.com>
2023-04-18 21:38:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Задача 4: НАпишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+Console.WriteLine("Введите первое число:");
 int a = int.Parse(Console.ReadLine());
-if (a % 2 == 0)
-{
-    Console.WriteLine("Да");
-}else{
-    Console.WriteLine("Нет");
-}
-// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(max < b) max = b;
+if(max < c) max = c;
+System.Console.WriteLine("Максимальное число:" +max);
