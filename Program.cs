1d7764a8935864175c687dea639d2094248e60a7
shnitkov <129221961+shnitkov@users.noreply.github.com>
2023-04-18 21:14:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Задание 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a < d)
+{
+    Console.WriteLine(b);
+}else{
+    Console.WriteLine(a);
+}
